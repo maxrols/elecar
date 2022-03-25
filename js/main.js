cbc738c6034cd34e7519bc670ed97264bf4bacbb
Max <example@gmail.com>
2022-03-25 22:05:09 +0100

    First section

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const showBtn = document.querySelector('.nav__toggle');
 const navMenu = document.querySelector('.nav__menu');
 const closeBtn = document.querySelector('.nav__close');
 const navLinks = document.querySelectorAll('.nav__link');
+const header = document.querySelector('.header');
 
 showBtn.addEventListener('click', () => {
     navMenu.classList.add('show');
@@ -15,4 +16,12 @@ navLinks.forEach(link => {
     link.addEventListener('click', () => {
         navMenu.classList.remove('show');
     });
+});
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY >= 50) {
+        header.classList.add('scroll-header');
+    } else {
+        header.classList.remove('scroll-header');
+    }
 });
